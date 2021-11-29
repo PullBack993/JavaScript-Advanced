b37273639abf88448e825d9c7c600d3c8a7ea568
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2021-11-29 11:10:29 +0100

    add

---
+++ DOM - Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+  let firstNum = Number(document.getElementById("num1").value);
+  let secondNum = Number(document.getElementById("num2").value);
+  let result = firstNum + secondNum;
+  document.getElementById("sum").value = result
+}

01885737d17c01536ab7d53e547198a2b5dc07e7
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2021-12-02 11:07:37 +0100

    add

---
+++ DOM - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -0,0 +1,13 @@
+function extract(content) {
+  const text = document.getElementById(content).textContent;
+  const regex = /\((.+?)\)/gm;
+
+  let result = [];
+
+  let match = regex.exec(text);
+  while (match != null) {
+    result.push(match[1]);
+    match = regex.exec(text);
+  }
+  return result.join("; ");
+}

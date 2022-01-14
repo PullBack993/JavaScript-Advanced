574b16f015f5eeede41e4ce921b908b57fc78295
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-01-14 11:59:39 +0100

    add

---
+++ Function Advanced - Exercise/1.Sort-Array.js
@@ -0,0 +1,8 @@
+function solve(arr, orderType) {
+  return orderType === "asc"
+    ? arr.sort((a, b) => a - b)
+    : arr.sort((a, b) => b - a);
+}
+
+console.log(solve([14, 7, 17, 6, 8], "asc"));
+console.log(solve([14, 7, 17, 6, 8], "desc"));

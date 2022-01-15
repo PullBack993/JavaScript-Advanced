44aa2b64eea309d63d4d10a451467c4ba853a1de
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-01-15 13:20:06 +0100

    ADD

---
+++ Objects and Composition - Exercise/1.Calorie-Object.js
@@ -0,0 +1,12 @@
+function solve(arr){
+    let result = {};
+
+    for (let i = 0;i < arr.length; i+=2){
+        result[arr[i]] = Number(arr[i+1]);
+        // console.log(result[i])
+    }
+    return result;
+}
+
+
+console.log(solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));

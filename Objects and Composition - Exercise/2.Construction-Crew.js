4187d3ccdbf90993edaf971880f53f358aa65b27
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-01-16 13:10:14 +0100

    add

---
+++ Objects and Composition - Exercise/2.Construction-Crew.js
@@ -0,0 +1,23 @@
+function solve(worker){
+    if (worker.dizziness !== false){
+        worker.levelOfHydrated += 0.1*worker.weight * worker.experience;
+        worker.dizziness = false;
+    } else {
+        return worker;
+    }
+    return worker;
+}
+
+
+console.log(solve({ weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true }
+  ));
+
+
+  console.log(solve({ weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true }
+  ));

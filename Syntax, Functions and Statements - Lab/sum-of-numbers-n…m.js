f560f5aac0c0fd8024c96e70dda29d166702bf97
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-02-06 23:52:52 +0100

    add

---
+++ Syntax, Functions and Statements - Lab/sum-of-numbers-n…m.js
@@ -0,0 +1,14 @@
+function allNum(n, m){
+    let num1 = Number(n);
+    let num2 = Number(m)
+    let result = 0
+    for (let i = num1; i <= num2; i++){
+        result +=i
+    }
+    return result
+    
+}
+
+
+a = allNum(1, 5)
+console.log(a)
